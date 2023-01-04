ec40c4c3713070c788926d06271e6e611d3aef78
duongmanhhoang <manhhoang3151996@gmail.com>
2023-01-04 08:42:03 +0700

    get server

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "SERVER_ADDR: " . $_SERVER['SERVER_ADDR'];
+function getServer() {
+    if ($_SERVER['SERVER_ADDR'] === '172.31.42.64') return 'This is Green server';
+
+    return 'This is Blue server';
+}
+
+echo "SERVER_ADDR: " . getServer();
 echo "\n";
 echo "Delpoyed 1 time";
